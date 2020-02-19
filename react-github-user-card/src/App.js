e26fb5fccd9e3e2ba75c98b033ad3bd06e435715
Jeff Glanville <jeff.glanville@outlook.com>
2020-02-18 18:14:54 -0600

    add components and data

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Usercard from './components/Usercard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>user cards</h1>
+      <Usercard />
+    </div>
+  );
+}
+
+export default App;
