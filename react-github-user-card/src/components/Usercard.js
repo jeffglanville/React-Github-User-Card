5e72cb9897df799eed0b1c9203a8efff4ba64279
Jeff Glanville <jeff.glanville@outlook.com>
2020-02-18 18:55:55 -0600

    add fetch to Usercard

--- react-github-user-card/src/components/Usercard.js
+++ react-github-user-card/src/components/Usercard.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function usercard() {
-    return (
-        <div>
-            <p>userinfo</p>
-        </div>
-    )
+class Usercard extends Component {
+    constructor(){
+        super();
+        this.state={
+            followers: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/users/jeffglanville")
+        .then(res => this.setState({ followers: this.followers }))
+        .catch(err => console.log(err));
+    }
+
+    fetchFollowers = e => {
+        e.preventDefault();
+
+        fetch(`https://api.github.com/users/jeffglanville/${this.state.followers}`)
+        .then(followers => this.setState({ followers: this.followers }))
+        .catch(err => console.log(err));
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
+
+
+
+export default Usercard;
